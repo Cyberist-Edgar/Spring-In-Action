d1d35907381fb9bf1db78666aca2775cf358086e
Cyberist-Edgar <201648748@qq.com>
2020-07-02 20:48:13 +0800

    add matchers

--- Chapter 4/security-jpa/src/main/java/cn/edgar/security/config/SpringSecurityConfig.java
+++ Chapter 4/security-jpa/src/main/java/cn/edgar/security/config/SpringSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -26,5 +27,12 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService)
                 .passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").hasRole("USER")
+                .antMatchers("/", "/**").permitAll();
     }
 }
